b0b72101fc9d8d30f05d6877a854c9c5a7431744
Krishna Damarla <damarla.kk@gmail.com>
2014-12-27 12:39:24 -0600

    DB creation and unit testing DML

---
+++ app/src/androidTest/java/com/example/krishnadamarla/sunshine/test/TestDb.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.krishnadamarla.sunshine.test;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+import com.example.krishnadamarla.sunshine.data.WeatherContract.LocationEntry;
+import com.example.krishnadamarla.sunshine.data.WeatherContract.WeatherEntry;
+import com.example.krishnadamarla.sunshine.data.WeatherDbHelper;
+
+import java.util.Map;
+import java.util.Set;
+
+public class TestDb extends AndroidTestCase {
+
+    public static final String LOG_TAG = TestDb.class.getSimpleName();
+
+    public void testCreateDb() throws Throwable {
+        mContext.deleteDatabase(WeatherDbHelper.DATABASE_NAME);
+        SQLiteDatabase db = new WeatherDbHelper(
+                this.mContext).getWritableDatabase();
+        assertEquals(true, db.isOpen());
+        db.close();
+    }
+
+    /* TODO Uncomment for
+    4a - JUnit testing
+    https://www.udacity.com/course/viewer#!/c-ud853/l-1639338560/m-1633698603 */
+    public void testInsertReadDb() {
+
+        // If there's an error in those massive SQL table creation Strings,
+        // errors will be thrown here when you try to get a writable database.
+        WeatherDbHelper dbHelper = new WeatherDbHelper(mContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues values = getLocationValues();
+
+        long locationRowId;
+        locationRowId = db.insert(LocationEntry.TABLE_NAME, null, values);
+
+        // Verify we got a row back.
+        assertTrue(locationRowId != -1);
+        Log.d(LOG_TAG, "New row id: " + locationRowId);
+
+
+        // A cursor is your primary interface to the query results.
+        Cursor cursor = db.query(
+                LocationEntry.TABLE_NAME,  // Table to Query
+                null,
+                null, // Columns for the "where" clause
+                null, // Values for the "where" clause
+                null, // columns to group by
+                null, // columns to filter by row groups
+                null // sort order
+        );
+
+        // If possible, move to the first row of the query results.
+        if (!validateCursor(cursor, values)){
+            // That's weird, it works on MY machine...
+            fail("No values returned :(");
+        }
+
+        ContentValues weatherValues = getWeatherContentValues(locationRowId);
+
+
+        /**
+         * TODO YOUR CODE BELOW HERE FOR QUIZ
+         * QUIZ - 4a - InsertReadDbTest
+         * https://www.udacity.com/course/viewer#!/c-ud853/l-1639338560/e-1633698604/m-1633698605
+         **/
+
+        long weatherRowId = db.insert(WeatherEntry.TABLE_NAME, null, weatherValues);
+        assertTrue(weatherRowId != -1);
+        Log.d(LOG_TAG,"New weather id:" + weatherRowId);
+
+        Cursor weatherCursor = db.query(WeatherEntry.TABLE_NAME, null, null, null, null, null, null);
+        // If possible, move to the first row of the query results.
+        if (!validateCursor(weatherCursor, weatherValues)) {
+            fail("No weather values returned :(");
+        }
+
+        /* TODO Uncomment for
+        4a - JUnit testing
+        https://www.udacity.com/course/viewer#!/c-ud853/l-1639338560/m-1633698603*/
+        dbHelper.close();
+    }
+
+    private ContentValues getWeatherContentValues(long locationRowId) {
+        ContentValues weatherValues = new ContentValues();
+        weatherValues.put(WeatherEntry.COLUMN_LOC_KEY, locationRowId);
+        weatherValues.put(WeatherEntry.COLUMN_DATETEXT, "20141205");
+        weatherValues.put(WeatherEntry.COLUMN_DEGREES, 1.1);
+        weatherValues.put(WeatherEntry.COLUMN_HUMIDITY, 1.2);
+        weatherValues.put(WeatherEntry.COLUMN_PRESSURE, 1.3);
+        weatherValues.put(WeatherEntry.COLUMN_MAX_TEMP, 75.1);
+        weatherValues.put(WeatherEntry.COLUMN_MIN_TEMP, 65.1);
+        weatherValues.put(WeatherEntry.COLUMN_SHORT_DESC, "Asteroids");
+        weatherValues.put(WeatherEntry.COLUMN_WIND_SPEED, 5.5);
+        weatherValues.put(WeatherEntry.COLUMN_WEATHER_ID, 321);
+        return weatherValues;
+    }
+
+    public static final String TEST_CITY_NAME = "North Pole";
+
+    private ContentValues getLocationValues() {
+        // Test data we're going to insert into the DB to see if it works.
+        String testLocationSetting = "99705";
+        double testLatitude = 64.7488;
+        double testLongitude = -147.353;
+
+        // Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(LocationEntry.COLUMN_LOCATION_SETTING, testLocationSetting);
+        values.put(LocationEntry.COLUMN_CITY_NAME, TEST_CITY_NAME);
+        values.put(LocationEntry.COLUMN_COORD_LAT, testLatitude);
+        values.put(LocationEntry.COLUMN_COORD_LONG, testLongitude);
+        return values;
+    }
+
+
+    static boolean validateCursor(Cursor valueCursor, ContentValues expectedValues) {
+
+        assertTrue(valueCursor.moveToFirst());
+
+        Set<Map.Entry<String, Object>> valueSet = expectedValues.valueSet();
+        for (Map.Entry<String, Object> entry : valueSet) {
+            String columnName = entry.getKey();
+            int idx = valueCursor.getColumnIndex(columnName);
+            assertFalse(idx == -1);
+            String expectedValue = entry.getValue().toString();
+            assertEquals(expectedValue, valueCursor.getString(idx));
+        }
+        valueCursor.close();
+        return true;
+    }
+
+//    static final String TEST_LOCATION = "99705";
+//    static final String TEST_DATE = "20141205";
+
+}
